a9e89010a0fe216c07bcdbb98c1e836f96968097
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-08-28 10:06:08 +0700

    Birthday Cake Candles

---
+++ src/HackerRank/ProblemSolving/BirthdayCakeCandles.java
@@ -0,0 +1,53 @@
+package HackerRank.ProblemSolving;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result9 {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        // Write your code here
+        int maxVal = Collections.max(candles);
+        int total = 0;
+
+        for (Integer in : candles) {
+            if (in == maxVal) {
+                total++;
+            }
+        }
+
+        return total;
+    }
+}
+
+public class BirthdayCakeCandles {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result9.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
