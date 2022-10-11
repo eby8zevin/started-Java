7633f8a3ad7fdc208e696e8af8da703aadd1e641
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-10-11 19:38:42 +0700

    Sherlock and Squares

---
+++ src/HackerRank/ProblemSolving/SherlockAndSquares.java
@@ -0,0 +1,50 @@
+package HackerRank.ProblemSolving;
+
+import java.io.*;
+import java.util.stream.IntStream;
+
+class Result32 {
+
+    /*
+     * Complete the 'squares' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER a
+     *  2. INTEGER b
+     */
+
+    public static int squares(int a, int b) {
+        // Write your code here
+        return (int) ((Math.floor(Math.sqrt(b)) - (Math.ceil(Math.sqrt(a)))) + 1);
+    }
+}
+
+public class SherlockAndSquares {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int a = Integer.parseInt(firstMultipleInput[0]);
+
+                int b = Integer.parseInt(firstMultipleInput[1]);
+
+                int result = Result32.squares(a, b);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
