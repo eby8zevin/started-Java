dc27801504428b270b573f72098ac225e9918b48
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-10-14 19:16:32 +0700

    Cut the sticks

---
+++ src/HackerRank/ProblemSolving/CutTheSticks.java
@@ -0,0 +1,60 @@
+package HackerRank.ProblemSolving;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result34 {
+
+    /*
+     * Complete the 'cutTheSticks' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> cutTheSticks(List<Integer> arr) {
+        // Write your code here
+        // create list container to output
+        List<Integer> out = new ArrayList<>();
+
+        // until list is not empty
+        while (!arr.isEmpty()) {
+            // add current size to output
+            out.add(arr.size());
+            // remove all min entries in list for each iteration
+            arr.removeAll(Collections.singleton(Collections.min(arr)));
+        }
+        return out;
+    }
+}
+
+public class CutTheSticks {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result34.cutTheSticks(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
