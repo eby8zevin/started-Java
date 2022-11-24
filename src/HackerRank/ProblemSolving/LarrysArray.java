f0d6d6104d360019b369365c459b3832d7e91655
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-11-24 21:23:05 +0700

    Larry's Array

---
+++ src/HackerRank/ProblemSolving/LarrysArray.java
@@ -0,0 +1,68 @@
+package HackerRank.ProblemSolving;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result55 {
+
+    /*
+     * Complete the 'larrysArray' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY A as parameter.
+     */
+
+    public static String larrysArray(List<Integer> A) {
+        // Write your code here
+        int arr[] = A.stream().mapToInt(x -> x).toArray();
+        int inversionCount = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    inversionCount++;
+                }
+            }
+        }
+
+        if (inversionCount % 2 == 0) {
+            return "YES";
+        }
+
+        return "NO";
+    }
+
+}
+
+public class LarrysArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> A = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                String result = Result55.larrysArray(A);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
