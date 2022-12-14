55bad40cd585c4705c66d27e256838d4bc0db136
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-12-14 20:31:45 +0700

    Two Strings

---
+++ src/HackerRank/ProblemSolving/TwoStrings.java
@@ -0,0 +1,53 @@
+package HackerRank.ProblemSolving;
+
+import java.io.*;
+import java.util.stream.IntStream;
+
+class Result68 {
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static String twoStrings(String s1, String s2) {
+        // Write your code here
+        String answer = "NO";
+        if (s2.matches("[a-zA-Z]*[" + s1 + "][a-zA-Z]*")) {
+            answer = "YES";
+        }
+
+        return answer;
+    }
+}
+
+public class TwoStrings {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+
+                String s2 = bufferedReader.readLine();
+
+                String result = Result68.twoStrings(s1, s2);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
