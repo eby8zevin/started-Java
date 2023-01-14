70aedb58910b6da27a888809829fed22750bf3f8
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-01-14 20:23:37 +0700

    Intro to Tutorial Challenges

---
+++ src/HackerRank/ProblemSolving/IntroToTutorialChallenges.java
@@ -0,0 +1,55 @@
+package HackerRank.ProblemSolving;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result79 {
+
+    /*
+     * Complete the 'introTutorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER V
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int introTutorial(int V, List<Integer> arr) {
+        // Write your code here
+        int result = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (V == arr.get(i)) {
+                result = i;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
+
+public class IntroToTutorialChallenges {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int V = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result79.introTutorial(V, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
